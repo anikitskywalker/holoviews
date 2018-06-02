96c608d93f8126b7380c63fd17de669dbb6e2fe9
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-06-02 13:57:40 +0100

    Added unit tests for grid concatenation

--- tests/core/data/base.py
+++ tests/core/data/base.py
@@ -9,6 +9,7 @@ from unittest import SkipTest
 import numpy as np
 
 from holoviews import Dataset, HoloMap, Dimension
+from holoviews.core.data import concat
 from holoviews.element import Scatter, Curve
 from holoviews.element.comparison import ComparisonTestCase 
 
@@ -1075,3 +1076,16 @@ class GriddedInterfaceTests(object):
         values = np.sin(XS)
         sampled = Dataset((xs, ys, values), ['x', 'y'], 'z').sample(y=0)
         self.assertEqual(sampled, Curve((xs, values[0]), vdims='z'))
+
+    def test_concat_grid_3d(self):
+        array = np.random.rand(4, 5, 3, 2)
+        orig = Dataset((range(2), range(3), range(5), range(4), array), ['A', 'B', 'x', 'y'], 'z')
+        hmap = HoloMap({(i, j): self.element((range(5), range(4), array[:, :, j, i]), ['x', 'y'], 'z')
+                        for i in range(2) for j in range(3)}, ['A', 'B'])
+        ds = concat(hmap)
+        self.assertEqual(ds, orig)
+
+    def test_grid_3d_groupby_concat_roundtrip(self):
+        array = np.random.rand(4, 5, 3, 2)
+        orig = Dataset((range(2), range(3), range(5), range(4), array), ['A', 'B', 'x', 'y'], 'z')
+        self.assertEqual(concat(orig.groupby(['A', 'B'])), orig)
